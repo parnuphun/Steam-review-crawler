e2f24cc5d1390fed4759727260758cb583f81e15
parnuphun <parnuphun1598@gmail.com>
2022-12-26 18:44:24 +0700

    .

--- index.js
+++ index.js
@@ -1,102 +1,179 @@
 const puppeteer = require('puppeteer')
 
+let templateCaseName = 'twoSmall'
+let userStatus = 'online'
+
 async function scrap(){
     
     const browser = await puppeteer.launch(); // open brownser
     const page = await browser.newPage(); // open new tap 
     await page.goto('https://steamcommunity.com/app/730/reviews/?filterLanguage=all&p=1&browsefilter=mostrecent')
 
-    let data = []
-    let dataObj = {
-        username: '',
-        reivewPoN: '',
-        reviewText: '',
-        reviewDate: '',
-    }
+    let users = []
 
-    // row per page
-    const pageRowCount = await page.$$eval('#page1 > div', el => el.length)
-    console.log('pageRowCount ',pageRowCount); // number 
+    // จำนวนแถวทุ้งหมดใน 1 หน้า
+    const totolRowPage = await page.$$eval('#page1 > div', el => el.length)
+    console.log('page 1 have ',totolRowPage,' rows'); 
 
-    // * row have 2 type 
-    //  1. #page_?_row_?_template_twoSmall
-    //  2. #page_?_row_?_template_threeSmall
 
-    for(let pageRowIndex = 1 ; pageRowIndex <= pageRowCount ; pageRowIndex++){
-
-        // user per row
-        //default 1. twoSmall case
-        const userCount = await page.$$eval(`#page_1_row_${pageRowIndex}_template_twoSmall > div`, (el)=>{
-            // in user review must be attr data-panel 
-            return el.filter(el => el.hasAttribute('data-panel')).length
-        }).then((result)=>{
-            // 2. threeSmall Case 
-            if(result === 0 || result === undefined || result === null ){
-                return page.$$eval(`#page_1_row_${pageRowIndex}_template_threeSmall > div`, (el)=>{
-                    return el.filter(el => el.hasAttribute('data-panel')).length
-                })
-            }else{
-                return result
-            }
-        })
-        // console.log(userCount);
-        // loop user per col
-        for(let userIndex = 1; userIndex < userCount; userIndex++){
-            // loop user content (body / footer)
-            // * status 
-            //  1. online
-            //  2. in-game 
-            //  3. offline
-            for(let contentIndex = 1 ; contentIndex <= 2 ; contentIndex++){
-                // case 1 online (default)
-                let statusSelectorCheck = `#page_1_row_1_template_twoSmall > div:nth-child(${contentIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > div.apphub_CardContentAuthorName.online.ellipsis > a:nth-child(2)`
-                // chec user status
-                await page.$$eval(statusSelectorCheck , el => el.length)
-                    // clase 2 in-game
-                    .then((result)=>{
-                        if(result === 0){
-                            statusSelectorCheck = `#page_1_row_1_template_twoSmall > div:nth-child(${contentIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > div.apphub_CardContentAuthorName.in-game.ellipsis > a:nth-child(2)`
-                            return page.$$eval(statusSelectorCheck , el => el.length)
-                            // case 3 offline
-                            .then((result)=>{
-                                if(result === 0){
-                                    statusSelectorCheck = `#page_1_row_1_template_twoSmall > div:nth-child(${contentIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > div.apphub_CardContentAuthorName.offline.ellipsis > a:nth-child(2)`
-                                    return page.$$eval(statusSelectorCheck,el => el.length)
-                                }else{
-                                    return result
-                                }
-                            })
-                        }else{
-                            return result
-                        }
-                    })
-
-                let waitEL = await page.waitForSelector(statusSelectorCheck)
-                let text = await page.evaluate(el => el.textContent , waitEL)
-                // console.log(text);
-            }
-        }
+    for(let pageRowIndex = 1 ; pageRowIndex <= totolRowPage ; pageRowIndex++){
+        // จำนวน user ในแถว
+        const totalUserInRow = await checkTemplate(page,pageRowIndex)
+        console.log('page 1 row ', pageRowIndex ,' have ', totalUserInRow ,' user');
         
-        // get vote review
-        // let selector = `#page_1_row_1_template_twoSmall > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.vote_header > div.reviewInfo > div.thumb > img`
-        // let element = await page.waitForSelector(selector)
-        // let text = await page.$eval(selector, el => el.getAttribute('src'));
-        // if(text.includes('thumbsUp')){
-        //     text = 'positive'
-        // }else if(text.includes('thumbsDown')){
-        //     text = 'negative'
-        // }else{
-        //     text = 'cant read value'
-        // }
-        // console.log(text);
+        for(let userIndex = 1; userIndex <= totalUserInRow; userIndex++){
+            // console.log('user คนที่ ', userIndex);
+            let user = {}
+            
+            // get username 
+            let statusSelectorCheck = await checkUserStatus(page,pageRowIndex,userIndex)
+            let waiteStatusSL = await page.waitForSelector(statusSelectorCheck)
+            let username = await page.evaluate(el => el.textContent , waiteStatusSL)
+            user.username = username
+            
+            
+            // get date
+            let dateSL = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.apphub_CardTextContent > div`
+            let waitDateSL = await page.waitForSelector(dateSL)
+            let date = await page.evaluate(el => el.textContent , waitDateSL)
+            user.date = date
+
+            // get review 
+            let reviewSL = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.apphub_CardTextContent`
+            let waitReviewSL = await page.waitForSelector(reviewSL)
+            let review = await page.evaluate(el => el.textContent , waitReviewSL)
+            user.review = review
+
+            // get avartar 
+            let avartarSL = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > a > div.appHubIconHolder.${userStatus} > img`
+            await page.waitForSelector(avartarSL)
+            let avartar = await page.$eval(avartarSL, el => el.getAttribute('src'))
+            user.avartar = avartar 
+
+            // get vote review
+            let voteSL = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.vote_header > div.reviewInfo > div.thumb > img`
+            await page.waitForSelector(voteSL)
+            let vote = await page.$eval(voteSL,el => el.getAttribute('src'));
+            if(vote.includes('thumbsUp')){
+                vote = 'positive'
+            }else if(vote.includes('thumbsDown')){
+                vote = 'negative'
+            }else{
+                vote = 'cant read value'
+            }
+            user.vote = vote
+
+            users.push(user)
+
+        }
+            
+
     }
 
     await browser.close() // close 
-    console.log('ending');
-}
-
-function checkRowCase(page , row){
-
+    console.log(users);
+    console.log('end');
 }
 
 scrap()
+
+// *** row type
+//  1. #page_?_row_?_template_twoSmall
+//  2. #page_?_row_?_template_threeSmall
+//  3. #page_?_row_?_template_smallFallbac 
+//  4. #page_?_row_?_template_mediumFallback
+//  5. #page_?_row_?_template_largeFallback
+async function checkTemplate(page,pageRowIndex){
+    templateCaseName = 'twoSmall'
+    return await page.$$eval(`#page_1_row_${pageRowIndex}_template_twoSmall > div`, (el)=>{
+        return el.filter(el => el.hasAttribute('data-panel')).length  // in user review must be attr data-panel 
+    })
+    // 2. threeSmall Case 
+    .then((result)=>{
+        if(result === 0 || result === undefined || result === null ){
+            templateCaseName = 'threeSmall'
+            return page.$$eval(`#page_1_row_${pageRowIndex}_template_threeSmall > div`, (el)=>{
+                return el.filter(el => el.hasAttribute('data-panel')).length
+            })
+        }else{
+            return result
+        }
+    // 3. smallFallback
+    }).then((result)=>{
+        if(result === 0 || result === undefined || result === null ){
+            templateCaseName = 'smallFallback'
+            return page.$$eval(`#page_1_row_${pageRowIndex}_template_smallFallback > div`, (el)=>{
+                return el.filter(el => el.hasAttribute('data-panel')).length
+            })
+        }else{
+            return result
+        }
+    })
+    // 4. mediumFallback
+    .then((result)=>{
+        if(result === 0 || result === undefined || result === null ){
+            templateCaseName = 'mediumFallback'
+            return page.$$eval(`#page_1_row_${pageRowIndex}_template_mediumFallback > div`, (el)=>{
+                return el.filter(el => el.hasAttribute('data-panel')).length
+            })
+        }else{
+            return result
+        }
+    })
+    // 5. largeFallback
+    .then((result)=>{
+        if(result === 0 || result === undefined || result === null ){
+            templateCaseName = 'largeFallback'
+            return page.$$eval(`#page_1_row_${pageRowIndex}_template_largeFallback > div`, (el)=>{
+                return el.filter(el => el.hasAttribute('data-panel')).length
+            })
+        }else{
+            return result
+        }
+    })
+}
+
+// *** user status type 
+//  1. online
+//  2. offline
+//  3. in-game 
+async function checkUserStatus(page,pageRowIndex,userIndex){
+    let statusSelectorCheck = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > div.apphub_CardContentAuthorName.online.ellipsis > a:nth-child(2)`            
+
+    // case 1 
+    userStatus = 'online'
+    await page.$$eval(statusSelectorCheck , el => el.length)
+    // case 2 in-game
+    .then((result)=>{
+        if(result === 0){
+            userStatus = 'in-game'
+            statusSelectorCheck = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > div.apphub_CardContentAuthorName.in-game.ellipsis > a:nth-child(2)`
+            return page.$$eval(statusSelectorCheck , el => el.length)
+        }else{
+            return result
+        }
+    })
+    // case 3 offline
+    .then((result)=>{
+        if(result === 0){
+            userStatus = 'offline'
+            statusSelectorCheck = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > div.apphub_CardContentAuthorName.offline.ellipsis > a:nth-child(2)`
+            return page.$$eval(statusSelectorCheck,el => el.length)
+        }else{
+            return result
+        }
+    })
+
+    return statusSelectorCheck
+}
+
+async function voteSelector (page,pageRowIndex,userIndex){
+    let voteSL = `#page_1_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.vote_header > div.reviewInfo > div.thumb > img`
+    await page.$$eval(voteSL , el => el.length).then((result)=>{
+        if(result === 0){
+            console.log( 'no url !!!!!!');
+        }
+    })
+
+    return voteSL
+}
