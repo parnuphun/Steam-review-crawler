11d67483b46742c8b8d5944cd40c6d237763d621
parnuphun <parnuphun1598@gmail.com>
2023-02-09 16:35:00 +0700

    Config Tailwind And EJS

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const scrap = require('./src/scrap')
 const express = require('express')
+const path = require('path')
 const app = express()
+const port = 4020
 
 // scrap()
+app.set('views', path.join(__dirname, 'views'))
+app.set ("view engine", "ejs" )
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.listen(4000 || 3030 , ()=>{ 
-    console.log(`server runing at port ${4000}...`)
+app.get('/',(req, res)=>{
+    res.render('index')
+})
+
+app.listen(port || 3030 , ()=>{ 
+    console.log(`server runing at port ${port} ...`)
 })
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs"],
+  theme: {
+    screens: {
+      'MB' : '200px',
+      'TL' : '640px',
+      'LT': '1024px',
+      'DT' : '1536px',
+    },  
+    extend: {},
+  },
+  plugins: [],
+}
