cca7045ae0aee6f854b692b057ea67ead3d71143
parnuphun <parnuphun1598@gmail.com>
2023-02-10 23:25:31 +0700

    Format

--- src/scrap.js
+++ src/scrap.js
@@ -1,119 +1,115 @@
 const puppeteer = require('puppeteer')
-const moment = require('moment')
-const exportCsv = require('./exportCsvFile')
 
-let templateCaseName = 'twoSmall'
-let userStatus = 'online'
-let pageIndex = 1
-let continouse = true
-let userCount = 1
+// default values
+let templateCaseName = 'twoSmall' // template Type 
+let userStatus = 'online' // user status
+let pageIndex = 1 // Total Pages
+let continouse = true // Start Loop 
+let userCount = 1 // User Count 
+let maxData = 1000 // max data scrap 
 
-async function scrap(steamUrl){
-    // open 
-    const browser = await puppeteer.launch({ 
-        headless: false,
+async function scrap(steamUrl , limit){
+
+    const browser = await puppeteer.launch({ // Start Bronser
+        headless: true, // open chromium
         timeout: 10000
     });
-    const page = await browser.newPage(); 
-    // await page.goto('https://steamcommunity.com/app/730/reviews/?filterLanguage=all&p=1&browsefilter=mostrecent')
-    await page.goto(steamUrl)
+    const page = await browser.newPage(); // Create Page
+    await page.goto(steamUrl) // Go to website
+    page.on('close', () => { // When Closed The Bronwser (Headless = false)
+        console.log('Page closed ....');
+    });
+    await btnGate(page) // Check Button Gate if game need to validate age !
 
-    
-    // check button cover page
-    // await btnGate(page)
-
-    let users = []
+    let users = [] 
     while(continouse){
-        // จำนวนแถวทุ้งหมดใน 1 หน้า
-        const totolRowPage = await getTotalRowInPage(page)
-        // console.log('page '+pageIndex+' have ',totolRowPage,' rows'); 
-        // await autoSrollPage(page)
-        
+        console.log(continouse);
+        const totolRowPage = await getTotalRowInPage(page) // จำนวนแถวทุ้งหมดใน 1 หน้า
         for(let pageRowIndex = 1 ; pageRowIndex <= totolRowPage ; pageRowIndex++){
-            // จำนวน user ในแถว
-            const totalUserInRow = await checkTemplate(page,pageRowIndex)
-            // console.log('page '+pageIndex + ' row ', pageRowIndex ,' have ', totalUserInRow ,' user');
-            
+            const totalUserInRow = await checkTemplate(page,pageRowIndex)  // จำนวน user ในแถว
             for(let userIndex = 1; userIndex <= totalUserInRow; userIndex++){
-                // console.log('user คนที่ ', userIndex);
-                let user = {}
-
-                // get date
-                let dateSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.apphub_CardTextContent > div`
-                let waitDateSL = await page.waitForSelector(dateSL)
-                let date = await page.evaluate(el => el.textContent , waitDateSL)
-                if(date.includes('2021')){
-                    continouse = false 
-                    break ;
+                let user = {} // user Object
+                try{
+                    // get date
+                    let dateSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.apphub_CardTextContent > div`
+                    let waitDateSL = await page.waitForSelector(dateSL)
+                    let date = await page.evaluate(el => el.textContent , waitDateSL)
+                    if(date.includes('Posted: ') || date.includes('โพสต์: ')){
+                        if(date.includes('Posted: '))   date = date.replace('Posted: ','') + ' ' + new Date().getFullYear()
+                        else                            date = date.replace('โพสต์: ','') + ' ' + new Date().getFullYear()
+                        user.date = date
+                    }
+                    // get username 
+                    let statusSelectorCheck = await checkUserStatus(page,pageRowIndex,userIndex)
+                    let waiteStatusSL = await page.waitForSelector(statusSelectorCheck)
+                    let username = await page.evaluate(el => el.textContent , waiteStatusSL)
+                    user.username = username
+                    // get review 
+                    let reviewSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.apphub_CardTextContent`
+                    let waitReviewSL = await page.waitForSelector(reviewSL)
+                    let review = await page.evaluate(el => el.textContent , waitReviewSL)
+                    user.review = review
+                    // get avartar 
+                    let avartarSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > a > div.appHubIconHolder.${userStatus} > img`
+                    await page.waitForSelector(avartarSL)
+                    let avartar = await page.$eval(avartarSL, el => el.getAttribute('src'))
+                    user.avartar = avartar 
+                    // get vote review
+                    let voteSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.vote_header > div.reviewInfo > div.thumb > img`
+                    await page.waitForSelector(voteSL)
+                    let vote = await page.$eval(voteSL,el => el.getAttribute('src'));
+                    if(vote.includes('thumbsUp')) vote = '+'
+                    else if(vote.includes('thumbsDown')) vote = '-'
+                    else vote = 'cant read value'
+                    user.vote = vote
+                    
+                    
+                    users.push(user) //push userObj to usersArr
+                    console.log(`page: ${pageIndex} row: ${pageRowIndex} user: ${userCount} ${username} success !!`); //review scraped log
+                    userCount++ //user scaped count for checking limit
+                }catch(err){
+                    console.log('err loop in');
+                    continouse = false
+                    break;
                 }
-                if(date.includes('ธันวาคม')){
-                    date = date.replace('โพสต์: ','') + ' 2565'
-                    date = moment.parseZone(date,'DD MMMM YYYY','th')
-                    user.date = moment(date).add('-543','year').format('L')
-                }else{
-                    date = date.replace('Posted: ','') + ' 2022'
-                    date = moment.parseZone(date,'DD MMMM YYYY','en')
-                    user.date = moment(date).format('L')
-                }
-
-                // get username 
-                let statusSelectorCheck = await checkUserStatus(page,pageRowIndex,userIndex)
-                let waiteStatusSL = await page.waitForSelector(statusSelectorCheck)
-                let username = await page.evaluate(el => el.textContent , waiteStatusSL)
-                user.username = username
-                
-                // get review 
-                let reviewSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.apphub_CardTextContent`
-                let waitReviewSL = await page.waitForSelector(reviewSL)
-                let review = await page.evaluate(el => el.textContent , waitReviewSL)
-                user.review = review
-                
-                // get avartar 
-                let avartarSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentAuthorBlock.tall > div.apphub_friend_block_container > div > a > div.appHubIconHolder.${userStatus} > img`
-                await page.waitForSelector(avartarSL)
-                let avartar = await page.$eval(avartarSL, el => el.getAttribute('src'))
-                user.avartar = avartar 
-                
-                // get vote review
-                let voteSL = `#page_${pageIndex}_row_${pageRowIndex}_template_${templateCaseName} > div:nth-child(${userIndex+1}) > div.apphub_CardContentMain > div.apphub_UserReviewCardContent > div.vote_header > div.reviewInfo > div.thumb > img`
-                await page.waitForSelector(voteSL)
-                let vote = await page.$eval(voteSL,el => el.getAttribute('src'));
-                if(vote.includes('thumbsUp')){
-                    vote = '+'
-                }else if(vote.includes('thumbsDown')){
-                    vote = '-'
-                }else{
-                    vote = 'cant read value'
-                }
-                user.vote = vote
-                
-                users.push(user)
-                console.log(`page: ${pageIndex} row: ${pageRowIndex} user: ${userCount} ${username} success !!`);
-                userCount++
             }
         }
-        // if(pageIndex === 10){
-        //     break;
-        // }
-        previousHeight = await page.evaluate('document.body.scrollHeight')
-        try {
-            await page.evaluate(`window.scrollTo(0, document.body.scrollHeight)`,{ timeout: 3000 });
-        } catch (error) {
-            break ;
+
+        if(((userCount >= limit) || (userCount >= maxData)) && ((limit != '') || (limit != 0) || (limit != '0'))){ // Check Limit
+            console.log('exceeded limit ')
+            continouse = false 
+            break 
+        }else{        
+            previousHeight = await page.evaluate('document.body.scrollHeight')   
+            try {
+                await page.evaluate(`window.scrollTo(0, document.body.scrollHeight)`,{ timeout: 10000 })
+            } catch (error) {
+                console.log('timeout 1 ')
+                continouse = false
+                break 
+            }
+            try {
+                await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`,{ timeout: 10000 })
+            } catch (error) {
+                console.log('timeout 2 no data or loading element slow')
+                continouse = false
+                break 
+            }
+            pageIndex ++
         }
-        try {
-            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`,{ timeout: 1000 })
-        } catch (error) {
-            break ;
-        }
-        pageIndex ++
         
     }
+    // await page.close()
     await browser.close() // close 
-    // console.log(users);
-    console.log('Total Reviews : ',users.length);
-    // await exportCsv(users)
-    console.log('end');
+    // Log Data After Scraped 
+    console.log('Total Reviews : ',users.length)
+    console.log('end')
+    // Reset Default Data Before Return Data 
+    templateCaseName = 'twoSmall'  
+    userStatus = 'online'  
+    pageIndex = 1  
+    continouse = true  
+    userCount = 1 
     return users
 }
 
@@ -207,15 +203,16 @@ async function checkUserStatus(page,pageRowIndex,userIndex){
     return statusSelectorCheck
 }
 
-// get total row per page
+// Get Total row per page
 async function getTotalRowInPage(page){
     return page.$$eval(`#page${pageIndex} > div`, el => el.length)
 }
 
+// Check Button Gate Because some game need to validate
 async function btnGate(page){
     return page.$$eval(`#age_gate_btn_continue` , el => el.length).then(async (result)=>{
         if(result === 0){
-            continouse = false
+            continouse = true
         }else {
             const btnGate = await page.$('#age_gate_btn_continue')
             await btnGate.evaluate( btnGate => btnGate.click())
